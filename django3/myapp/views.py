02551b5925e8a1e341fe927a83363bf3157867bf
Бурхон Мелиев <burxonjonmeliyev@gmail.com>
2026-04-18 14:29:23 +0500

    yangilanish
    yangilanish

--- django3/myapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    return render(request, 'home.html')
-
-def about(request):
-    return HttpResponse('<h1>Haqida Sahifasi</h1><p>Bu mening Django loyihasi haqida sahifasi.</p>')
