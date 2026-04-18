be13f742dcf4ae0923008dbf5369377274db33a8
Бурхон Мелиев <burxonjonmeliyev@gmail.com>
2026-04-18 15:56:07 +0500

    yangilanish
    yangilanish

---
+++ django-5-dars/myproject/myapp/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Post
+
+
+# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at')
+    list_filter = ('author', 'created_at')
+    search_fields = ('title', 'content', 'author__username')
+    ordering = ('-created_at',)
